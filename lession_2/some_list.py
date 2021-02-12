e192c7bcac63ebea0d49cd346968028df70c57fc
Richard_Ehrlich <77030634+happyehrlich@users.noreply.github.com>
2021-02-12 23:50:28 +0300

    в процессе

--- lession_2/some_list.py
+++ lession_2/some_list.py
@@ -1,21 +1,32 @@
 
 #some_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-some_list = ['в', '5', 'часов', '17', '+5']
-
+#some_list = ['в', '5', 'часов', '17', '+5']
+#some_list = ['в', '5', 'часов', '17', '+5']
+some_list = ['5', 'часов', '+5']
+some_list.reverse()
+print(some_list)
 for i in range(0, len(some_list)):
     temp_item = some_list[i]
     print('получаем первую строку: ' ,temp_item)
     temporary_list = list(temp_item)
     print('получаем временный список из строки: ' ,temporary_list)
-    count = None
+    count = 0
     index_list = []
     general_index_list = []
     for index, value in enumerate(temporary_list):
+        print('проверка индекса и значения: ', index, value)
         if value.isdigit():
+            print('проверочный вывод, если значение является числом')
             count += 1
-            index_list = index
+            index_list = str(index)
+            print(index_list)
             general_index_list = index
         else:
+            print('проверочный вывод, если значение является не числом')
             general_index_list = index
     if count == 1:
-        temporary_list.insert(temporary_list[0], 0)
+        qwe = index_list[0]
+        print(type(qwe))
+        print(type(temporary_list))
+        temporary_list.insert(int(qwe), 0)
+        print('проверочный вывод, temporary_list: ', temporary_list)
