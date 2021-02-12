1605fdae2b6ef0f239af2d17114bb3806908916e
Richard_Ehrlich <77030634+happyehrlich@users.noreply.github.com>
2021-02-13 00:56:46 +0300

    правки

--- lession_2/some_list.py
+++ lession_2/some_list.py
@@ -1,35 +1,34 @@
+#
+#
+#
+some_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
-#some_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
-#some_list = ['в', '5', 'часов', '17', '+5']
-#some_list = ['в', '5', 'часов', '17', '+5']
-some_list = ['5', 'часов', '+5']
-some_list.reverse()
+
+#
+def get_modofid_list():
+    for i in range(len(some_list)):
+        temp_item = some_list[i]
+        temporary_list = list(temp_item)
+        count = 0
+        index_list = []
+        general_index_list = []
+        for index, value in enumerate(temporary_list):
+            if value.isdigit():
+                count += 1
+                index_list = str(index)
+                general_index_list = index
+            else:
+                general_index_list = index
+
+        if count == 1:
+            qwe = index_list[0]
+            temporary_list.insert(int(qwe), '0')
+            char_for_join = ''
+            output1 = char_for_join.join(temporary_list)
+            some_list[i] = output1
+
+
+#
+
+get_modofid_list()
 print(some_list)
-for i in range(0, len(some_list)):
-    temp_item = some_list[i]
-    print('получаем первую строку: ' ,temp_item)
-    temporary_list = list(temp_item)
-    print('получаем временный список из строки: ' ,temporary_list)
-    count = 0
-    index_list = []
-    general_index_list = []
-    for index, value in enumerate(temporary_list):
-        print('проверка индекса и значения: ', index, value)
-        if value.isdigit():
-            print('проверочный вывод, если значение является числом')
-            count += 1
-            index_list = str(index)
-            print(index_list)
-            general_index_list = index
-        else:
-            print('проверочный вывод, если значение является не числом')
-            general_index_list = index
-    if count == 1:
-        qwe = index_list[0]
-        print(type(qwe))
-        print(type(temporary_list))
-        temporary_list.insert(int(qwe), '0')
-        print('проверочный вывод, temporary_list: ', temporary_list)
-        char_for_join = ''
-        output1 = char_for_join.join(temporary_list)
-        print(output1)
