baaccbf26987ea2b3ff8576fe98c2f9743f9d0fd
Richard_Ehrlich <77030634+happyehrlich@users.noreply.github.com>
2021-02-15 11:56:51 +0300

    в процессе 4 task

--- lession_2/task_4.py
+++ lession_2/task_4.py
@@ -3,3 +3,9 @@
 #
 # прочитать про приведение чисел к двум разрядам
 # деление числа в строке через запятую
+from random import random
+
+
+price_list = []
+while len(price_list) < 20:
+	price_list = round((random() * 89), 2)
