138cf8aedd1d88ca0b0de790657a0daa253c9353
Richard_Ehrlich <77030634+happyehrlich@users.noreply.github.com>
2021-02-15 22:33:16 +0300

    этап А - 4 task - закончен

--- lession_2/task_4.py
+++ lession_2/task_4.py
@@ -6,6 +6,46 @@
 from random import random
 
 
-price_list = []
-while len(price_list) < 20:
-	price_list = round((random() * 89), 2)
+# получаем прайс лист
+def get_random_list():
+	price_list_random = []
+	while len(price_list_random) < 20:
+		price_list_random.append(round((random() * 89), 2))
+	return price_list_random
+
+
+#################################
+
+price_list = get_random_list() # получаем прайс лист
+print(id(price_list))
+# делаем массив многомерным, не пересоздавая объекта
+for i in range(len(price_list)):
+	temp_var = str(price_list[i])
+	temp_var = temp_var.split('.')
+	price_list[i] = temp_var
+print(price_list)
+
+# дополняем одноразрядные цифры нулём, формируем правильный список цен
+for index in range(len(price_list)):
+	temp_some_num = price_list[index]
+	for value in range(1, len(temp_some_num), 2):
+		item = temp_some_num[value]
+		if len(item) < 2:
+			item = '0' + item
+			temp_some_num[value] = item
+	# temp_some_num[0] = f'{temp_some_num[0]} руб'
+	# temp_some_num[1] = f'{temp_some_num[1]} коп'
+	# temp_some_num = ' '.join(temp_some_num)
+	temp_some_num = f'{temp_some_num[0]} руб {temp_some_num[1]} коп'
+	price_list[index] = temp_some_num
+
+
+# получаем cтроку из списка не пересоздавая объекта
+# price_list = ', '.join(price_list)
+while True:
+	if len(price_list) > 1:
+		temp_var_for_string = f'{price_list.pop(0)}, {price_list.pop(0)}'
+		price_list.insert(0, temp_var_for_string)
+	else:
+		break
+print(price_list)
