3c6f22ac0e4c0f9a351e82e9faa72066866dda20
Richard_Ehrlich <77030634+happyehrlich@users.noreply.github.com>
2021-02-18 22:35:27 +0300

    redoing my homework

--- lesson_2/task_4.py
+++ lesson_2/task_4.py
@@ -1,71 +1,37 @@
 #
 #
 #
-from random import random
+list_of_employees = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
 
 
-# получаем прайс лист
-def get_random_list():
-	price_list_random = []
-	while len(price_list_random) < 20:
-		price_list_random.append(round((random() * 89), 2))
-	return price_list_random
+# проверяем любой список на присутствие цифр signed и unsigned
+def checking_for_a_number(user_any_list):
+	count = 0
+	total = 0
+	signed_int_number = []
+	for index in range(len(user_any_list)):
+		temp_list = list(user_any_list[index])
+		for value in range(len(temp_list)):
+			if temp_list[value] == '-':
+				if temp_list[value + 1].isdigit():
+					count += 1
+					signed_number = temp_list[value] + temp_list[value + 1]
+					signed_int_number = ''.join(signed_number)
+			elif not temp_list[value].isdigit():
+				continue
+			else:
+				total += 1
+	if total > 0:
+		print(f'В данном списке: {user_any_list}', f'\nВсего чисел ', {total}, f'из них отрицательных {count}:', '(', signed_int_number, ')')
+	else:
+		print(f'В данном списке: {user_any_list}', '\nНет чисел!')
 
 
-#################################
+checking_for_a_number(list_of_employees)
 
-
-def get_price_list(price_list):
-	# делаем массив многомерным, не пересоздавая объекта
-	for i in range(len(price_list)):
-		temp_var = str(price_list[i])
-		temp_var = temp_var.split('.')
-		price_list[i] = temp_var
-
-	# дополняем одноразрядные цифры нулём, формируем правильный список цен
-	index: int
-	for index in range(len(price_list)):
-		some_numbers = price_list[index]
-		for value in range(1, len(some_numbers), 2):
-			item = some_numbers[value]
-			if len(item) < 2:
-				item = '0' + item
-				some_numbers[value] = item
-		some_numbers = f'{some_numbers[0]} руб {some_numbers[1]} коп'
-		price_list[index] = some_numbers
-
-	# получаем cтроку из списка не пересоздавая объекта
-	while True:
-		if len(price_list) > 1:
-			temp_var_for_string = f'{price_list.pop(0)}, {price_list.pop(0)}'
-			price_list.insert(0, temp_var_for_string)
-		else:
-			break
-	return price_list
-
-
-#################################################
-print('получаем новый список и объект:')
-get_random_number: list
-get_random_number = get_random_list()
-print(get_random_number)
-print(id(get_random_number))
-
-print('\nполучаем изменённый список цен по первому условию, но не изменяя объект:')
-ready_price_list = get_price_list(get_random_number)
-print(ready_price_list)
-print(id(ready_price_list))
-
-print('#################################################')
-#
-# print('\nберём объект из первого условия и сортируем по второму условию:')
-# get_random_number = get_random_number[0]
-# print(get_random_number)
-# print(id(get_random_number))
-
-# print('\nполучаем изменённый список цен по второму условию, не изменяя объект:')
-# ready_price_list = get_price_list(get_random_number)
-# print(ready_price_list)
-# print(id(ready_price_list))
-
-print('#################################################')
+for i in range(len(list_of_employees)):
+	user_list = list_of_employees[i].split()
+	print(f'Привет, {user_list[-1].title()}')
+	user_list[-1] = user_list[-1].title()
+	list_of_employees[i] = ' '.join(user_list)
+print(f'Приводим список в порядок:\n{list_of_employees}')
---
+++ lesson_2/task_5.py
@@ -0,0 +1,71 @@
+#
+#
+#
+from random import random
+
+
+# получаем прайс лист
+def get_random_list():
+	price_list_random = []
+	while len(price_list_random) < 20:
+		price_list_random.append(round((random() * 89), 2))
+	return price_list_random
+
+
+#################################
+
+
+def get_price_list(price_list):
+	# делаем массив многомерным, не пересоздавая объекта
+	for i in range(len(price_list)):
+		temp_var = str(price_list[i])
+		temp_var = temp_var.split('.')
+		price_list[i] = temp_var
+
+	# дополняем одноразрядные цифры нулём, формируем правильный список цен
+	index: int
+	for index in range(len(price_list)):
+		some_numbers = price_list[index]
+		for value in range(1, len(some_numbers), 2):
+			item = some_numbers[value]
+			if len(item) < 2:
+				item = '0' + item
+				some_numbers[value] = item
+		some_numbers = f'{some_numbers[0]} руб {some_numbers[1]} коп'
+		price_list[index] = some_numbers
+
+	# получаем cтроку из списка не пересоздавая объекта
+	while True:
+		if len(price_list) > 1:
+			temp_var_for_string = f'{price_list.pop(0)}, {price_list.pop(0)}'
+			price_list.insert(0, temp_var_for_string)
+		else:
+			break
+	return price_list
+
+
+#################################################
+print('получаем новый список и объект:')
+get_random_number: list
+get_random_number = get_random_list()
+print(get_random_number)
+print(id(get_random_number))
+
+print('\nполучаем изменённый список цен по первому условию, но не изменяя объект:')
+ready_price_list = get_price_list(get_random_number)
+print(ready_price_list)
+print(id(ready_price_list))
+
+print('#################################################')
+#
+# print('\nберём объект из первого условия и сортируем по второму условию:')
+# get_random_number = get_random_number[0]
+# print(get_random_number)
+# print(id(get_random_number))
+
+# print('\nполучаем изменённый список цен по второму условию, не изменяя объект:')
+# ready_price_list = get_price_list(get_random_number)
+# print(ready_price_list)
+# print(id(ready_price_list))
+
+print('#################################################')
