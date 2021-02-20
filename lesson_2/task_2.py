213babf6a905d02162402c33ae4568195c6798fc
Richard_Ehrlich <77030634+happyehrlich@users.noreply.github.com>
2021-02-21 02:01:32 +0300

    поправил task_2.py и task_3.py

--- lesson_2/task_2.py
+++ lesson_2/task_2.py
@@ -1,7 +1,5 @@
-#
-#
-#
 some_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+
 
 # функция добавляет "0" перед числом, если оно состоит из одного разряда
 def get_modify_list():
@@ -9,17 +7,14 @@ def get_modify_list():
         temp_item = some_list[i]
         temporary_list = list(temp_item)
         count = 0
-        index_list = []
+        index_list = 0
         for index_variable, value in enumerate(temporary_list):
             if value.isdigit():
                 count += 1
-                index_list = str(index_variable)
-
+                index_list = index_variable
         if count == 1:
-            qwe = index_list[0]
-            temporary_list.insert(int(qwe), '0')
-            char_for_join = ''
-            output1 = char_for_join.join(temporary_list)
+            temporary_list.insert(index_list, '0')
+            output1 = ''.join(temporary_list)
             some_list[i] = output1
 
 
@@ -27,9 +22,7 @@ def get_modify_list():
 def inserting_a_quotation_mark_character():
     total_index = 0  # переменная для контроля шага итерации после увеличения длинны массива (чтобы не застревал на ковычках)
     for index in range(len(some_list)):
-        temp_item1 = some_list[total_index]
-        temporary_list1 = list(temp_item1)
-        count1 = [value for value in temporary_list1 if value.isnumeric()]
+        count1 = [value for value in some_list[total_index] if value.isnumeric()]
         if count1:
             some_list.insert(total_index, '"')
             some_list.insert(total_index + 2, '"')
@@ -51,11 +44,11 @@ def get_any_character(any_list, any_symbol):
     return symbol_list
 
 
-print("\nПрограмме дан список: ", some_list)  # показываем не изменённый список
-get_modify_list()  # Вызов модификации - первый этап
-print("По условию, мы добавляем '0' к числам из этого списка, у которых один разряд", "\n:", some_list, '\n')  # показываем изменённый список
-inserting_a_quotation_mark_character()  # Вызов модификации - второй этап
-print("Далее обрамляем каждое число в ковычки: ")  # показываем изменённый список
+print(f'\nПрограмме дан список: {some_list}')
+get_modify_list()
+print(f'По условию, мы добавляем "0" к числам из этого списка, у которых один разряд:\n{some_list}')
+inserting_a_quotation_mark_character()
+print("Далее обрамляем каждое число в ковычки: ")
 
 while some_list.count('"') > 0:
     pair_symbol = get_any_character(some_list, '"')
@@ -64,7 +57,7 @@ while some_list.count('"') > 0:
         list_reload += some_list.pop(pair_symbol[0])
     list_reload = ''.join(list_reload)
     some_list.insert(pair_symbol[0], list_reload)
-print(some_list, '\n')
+print(some_list)
 
 some_list = ' '.join(some_list)  # Получаем из списка строку
 print('Получаем из списка строку:', some_list)
--- lesson_2/task_3.py
+++ lesson_2/task_3.py
@@ -1,2 +1,63 @@
-input_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+some_list = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
 
+
+def modifying_the_list(any_list):
+	for i in range(len(any_list)):
+		temporary_str = any_list[i][:]
+		count = 0
+		index_container = 0
+		for index_variable, value in enumerate(temporary_str):
+			if value.isdigit():
+				count += 1
+				index_container = index_variable
+		if count == 1:
+			some_list[i] = temporary_str[:index_container] + '0' + temporary_str[index_container] + temporary_str[index_container + 1:]
+
+
+def insert_quotation_marks(total_number):
+	next_index = 0
+	count = 0
+	for index in range(len(some_list)):
+		for value in some_list[next_index]:
+			if value.isnumeric():
+				count = 1
+		if count:
+			some_list.insert(next_index, '"')
+			some_list.insert(next_index + 2, '"')
+			next_index += 3
+			total_number += 1
+		else:
+			next_index += 1
+		count = 0
+	return total_number
+
+
+def get_last_string(number_of_cycles):
+	while True:
+		try:
+			where_char = some_list.index('"')
+		except ValueError:
+			where_char = 0
+		any_str = ''
+		if where_char:
+			for _ in range(number_of_cycles):
+				any_str += some_list.pop(where_char)
+			some_list.insert(where_char, any_str)
+		else:
+			break
+	last_string = ''
+	for value in some_list:
+		last_string += f'{value} '
+	return last_string
+
+
+print('Программе дан объект в виде списка:')
+print(some_list, 'id:', id(some_list))
+modifying_the_list(some_list)
+print('Модифицируем список не создавая новый объект и не создавая новый список:')
+print(some_list, 'id:', id(some_list))
+total_numbers = insert_quotation_marks(0)
+print(some_list, 'id:', id(some_list))
+print('Получаем строку соблюдая условия:')
+main_string = get_last_string(total_numbers)
+print(main_string)
