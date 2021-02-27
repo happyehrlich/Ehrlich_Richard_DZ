3ab714dd99b225d9e19c0c4d391478eb35208aa0
Richard_Ehrlich <77030634+happyehrlich@users.noreply.github.com>
2021-02-28 01:59:48 +0300

    непонятная трабла lesson_3 tak_4

--- lesson_3/task_4.py
+++ lesson_3/task_4.py
@@ -1,29 +1,29 @@
 def thesaurus_adv(*args):
 	user_dict = {}
+	count = 0
+	total = 0
 	for value in args:
-		print(type(args))
-		first_list = value.split(' ')
-		first_sign = first_list[-1][0]
-		print(first_sign, type(first_sign))
-		if first_sign in user_dict.keys():
+		print('\nполучили из args:', value, '\n')
+		temp_list = value.split(' ')
+		key_dict = temp_list[1][0]
+		print(key_dict, ' ===  новый ключ!!!')
+		list_values = ''
+		for item in args:
+			_i = item.split(' ')
+			print(_i)
+			if _i[1].startswith(key_dict):
+				list_values += f'{item}, '
+				print(key_dict, list_values, '\nНАПОЛНЕНИЕ СТРОКИ')
+			count += 1
+			print('ИТЕРАЦИЯ вложенного цикла НОМЕР:', count, '\n')
+		if key_dict in user_dict.keys():
 			break
-		next_list = list(filter(lambda item:item.startswith(first_sign)))
-		print(next_list)
+		else:
+			user_dict[key_dict] = list_values
+		print('\n===конец внешнего цикла===')
+		count = 0  # обнуляем счётчик вложенного цикла
+		total += 1
+		print('===ИТЕРАЦИЯ внешнего цикла НОМЕР:', total, '===')
 
 
-
-
-
-def sorted_dict_keys(some_dict):
-	temp_dict = {}
-	list_keys = list(some_dict.keys())
-	list_keys.sort()
-	for key in list_keys:
-		temp_dict[key] = some_dict[key]
-	return temp_dict
-
-
-any_dict = thesaurus_adv("Иван Сергеев", "Инна Серова", "Петр Алексеев", "Илья Иванов", "Анна Савельева")
-# print('Получаем словарь из списка:', any_dict)
-# sorted_dict = sorted_dict_keys(any_dict)
-# print('Отсортированный словарь:', sorted_dict)
+thesaurus_adv('Петр Алексеев', 'Иван Сергеев', 'Инна Серова', 'Илья Иванов', 'Анна Савельева')
